RonGokhale/android_hardware_qcom_camera

QCamera2: release metadata buffer in destructor if needed
Metadata buffers are allocated during camera open, but be released
during metadata stream stop/destroy.

In some cts test cases, there's only open/close operations without start
preview, so there's no chance to release metadata buffers.

We should check and release metadata buffers in destructor of
QCameraMetadataStreamMemory, in case they're not released yet.

CRs-Fixed: 958859
Change-Id: Ia1ef485dd9e29210ac1c5400b95dc86ab680fea5

QCamera2/HAL/QCameraMem.cpp
@@ -979,6 +979,10 @@ QCameraMetadataStreamMemory::QCameraMetadataStreamMemory(bool cached)
  *==========================================================================*/
 QCameraMetadataStreamMemory::~QCameraMetadataStreamMemory()
 {
+    if (mBufferCount > 0) {
+        CDBG_HIGH("%s, buf_cnt > 0, deallocate buffers now.\n", __func__);
+        deallocate();
+    }
 }
 
 /*===========================================================================

oshmoun/camera

QCamera2/HAL/QCameraMem.cpp
@@ -983,6 +983,10 @@ QCameraMetadataStreamMemory::QCameraMetadataStreamMemory(bool cached)
  *==========================================================================*/
 QCameraMetadataStreamMemory::~QCameraMetadataStreamMemory()
 {
+    if (mBufferCount > 0) {
+        ALOGE("%s, buf_cnt > 0, deallocate buffers now.\n", __func__);
+        deallocate();
+    }
 }
 
 /*===========================================================================